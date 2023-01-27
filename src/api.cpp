483fd0129692d46c1abe4be86fa7ee3efd7f78c7
alanrtao <alanrtao@outlook.com>
2023-01-27 01:12:16 -0500

    remove functional pattern and complete routing api

--- src/api.cpp
+++ src/api.cpp
@@ -1,33 +1,59 @@
 #include "api.h"
-#include "functional_helper.h"
 
-void examine_existing_webhook (dpp::cluster& bot, const dpp::webhook& wh, dpp::snowflake chan, bool& channel_has_webhook);
+bool examine_existing_webhook (dpp::cluster& bot, const dpp::webhook& wh, dpp::snowflake chan, bool channel_has_webhook);
+bool examine_existing_role (dpp::cluster& bot, const dpp::role& r, bool guild_has_role);
 void create_webhook (dpp::cluster& bot, const dpp::snowflake chan);
+void create_role (dpp::cluster& bot, const dpp::snowflake guild, const dpp::snowflake chan);
 
 void api::calls::route_here_call (const dpp::slashcommand_t& event, dpp::cluster& bot) {
     const auto channel{event.command.get_channel()};
     const auto guild{event.command.get_guild()};
 
-    event.reply("P4DC routed to "+channel.name+" :house: \nMake sure to run /"+api::create_role.route+" and /"+api::integration_help.route+" to create custom scripts for integrating Perforce triggers.");
+    event.reply(
+        ":anchor: P4DC routed to "+channel.name+"  \n"
+        "Make sure to run /"+api::create_role.route+" and /"+api::integration_help.route+" to create custom scripts for integrating Perforce triggers.\n"
+        ":point_right: I will send a message below if a webhook has been found / created...");
 
-    bot.get_guild_webhooks(channel.id, do_or_err<dpp::webhook_map>(
-        [&bot, &chan=channel.id](const dpp::webhook_map& m) {
+
+    const auto chan{channel.id};
+    bot.get_guild_webhooks(guild.id, [&bot, &chan](auto cb) {
+        if (cb.is_error()) {
+            bot.log(dpp::loglevel::ll_error, "Could not read webhooks: " + cb.get_error().message);
+            bot.message_create(dpp::message(chan, ":exclamation: Could not read webhooks from channel."));
+        } else {
+            auto m{std::get<dpp::webhook_map>(cb.value)};
             bool channel_has_webhook = false;
             for(const auto& [k, v] : m)
-                examine_existing_webhook(bot, v, chan, channel_has_webhook);
+                channel_has_webhook = examine_existing_webhook(bot, v, chan, channel_has_webhook);
             if (!channel_has_webhook)
                 create_webhook(bot, chan);
             else
                 bot.message_create(dpp::message(chan, "Webhook already exists on this channel."));
-        },
-        [&bot, &chan=channel.id](const std::string& err) {
-            bot.message_create(dpp::message(chan, ":exclamation: Could not read webhooks from channel."));
         }
-    ));
+    });
 }
 
 void api::calls::create_role_call (const dpp::slashcommand_t& event, dpp::cluster& bot) {
+    const auto channel{event.command.get_channel()};
+    const auto guild{event.command.get_guild()};
     
+    const auto chan{channel.id};
+    bot.roles_get(guild.id, [&bot, &chan](auto cb){
+        if (cb.is_error()) {
+            bot.log(dpp::loglevel::ll_error, "Could not read roles: " + cb.get_error().message);
+            bot.message_create(dpp::message(chan, ":exclamation: Could not read roles in this server."));
+        } else {
+            auto m{std::get<dpp::role_map>(cb.value)};
+            bool guild_has_role = false;
+            for(const auto& [k, v] : m)
+                guild_has_role = examine_existing_role(bot, v);
+            
+            if (!channel_has_webhook)
+                create_role(bot, chan);
+            else
+                bot.message_create(dpp::message(chan, "The" + api::names::role + " already exists on this channel."));
+        }
+    });
 }
 
 void api::calls::integration_help_call (const dpp::slashcommand_t& event, dpp::cluster& bot) {
@@ -38,27 +64,32 @@ bool is_my_role(const dpp::cluster& bot, const dpp::role& r) {
     return r.name == api::names::role && r.is_mentionable();
 }
 
-void examine_existing_roles (dpp::cluster& bot, const dpp::role& r, dpp::snowflake chan, bool& guild_has_role) {
+bool examine_existing_role (dpp::cluster& bot, const dpp::role& r, dpp::snowflake chan, bool guild_has_role) {
     if (is_my_role(bot, r)) {
         guild_has_role = true;
     }
+    return guild_has_role;
+}
+
+void create_role (dpp::cluster& bot, const dpp::snowflake guild, const dpp::snowflake chan) {
+    dpp::role new_role();
 }
 
 bool is_my_webhook(const dpp::cluster& bot, const dpp::webhook& wh) {
     return wh.name == api::names::webhook && wh.application_id == bot.me.id;
 }
 
-void examine_existing_webhook (dpp::cluster& bot, const dpp::webhook& wh, dpp::snowflake chan, bool& channel_has_webhook) {
+bool examine_existing_webhook (dpp::cluster& bot, const dpp::webhook& wh, dpp::snowflake chan, bool channel_has_webhook) {
     if (is_my_webhook(bot, wh)) {
         if (wh.channel_id == chan && !channel_has_webhook) {
-            std::cout<<"webhook already exists on channel "<<chan<<std::endl;
             channel_has_webhook = true;
         }
         else {
+            bot.message_create(dpp::message(wh.channel_id, ":point_right: " + api::names::webhook + " no longer needed and has been removed."));
             bot.delete_webhook(wh.id);
-            bot.message_create(dpp::message(wh.channel_id, ":point_right: " + api::names::webhook + " already exists on this channel"));
         }
     }
+    return channel_has_webhook;
 }
 
 void create_webhook (dpp::cluster& bot, const dpp::snowflake chan) {
@@ -66,12 +97,14 @@ void create_webhook (dpp::cluster& bot, const dpp::snowflake chan) {
     new_hook.name = api::names::webhook;
     new_hook.application_id = bot.me.id;
     new_hook.channel_id = chan;
-    bot.create_webhook(new_hook, do_or_err<dpp::webhook>(
-        [&bot, &chan](const dpp::webhook& wh) {
-            bot.message_create(dpp::message(chan, ":link: Webhook " + api::names::webhook + " created."));
-        },
-        [&bot, &chan](const std::string& err) {
+
+    bot.create_webhook(new_hook, [&bot, &chan](auto cb) {
+        if (cb.is_error()) {
+            bot.log(dpp::loglevel::ll_error, cb.get_error().message);
             bot.message_create(dpp::message(chan, ":exclamation: Webhook " + api::names::webhook + " could not be created."));
+        } else {
+            auto wh{std::get<dpp::webhook>(cb.value)};
+            bot.message_create(dpp::message(wh.channel_id, ":link: Webhook " + api::names::webhook + " created."));
         }
-    ));
+    });
 }
