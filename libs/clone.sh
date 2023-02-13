0a0608252b8fdaf808cc02aef19710cc036b90ff
alanrtao <alanrtao@outlook.com>
2023-02-13 12:02:24 -0500

    prebuilt libdpp

--- libs/clone.sh
+++ libs/clone.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # git clone https://github.com/SRombauts/SQLiteCpp.git
-git clone https://github.com/brainboxdotcc/DPP.git
+# git clone https://github.com/brainboxdotcc/DPP.git
 
+rm -rf DPP
+dpkg-deb -xv libdpp-10.0.22-linux-x64.deb DPP_
+mv DPP_/usr DPP
+rm -rf DPP_
 sudo apt install g++ gdb cmake zlib1g-dev libarchive-dev libssl-dev
