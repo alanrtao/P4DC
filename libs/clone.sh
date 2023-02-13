ed1c274ad8c5dc5ad73097837c6d7736bc37c349
alanrtao <alanrtao@outlook.com>
2023-02-13 14:35:37 -0500

    package install fix

--- libs/clone.sh
+++ libs/clone.sh
@@ -7,4 +7,4 @@ rm -rf DPP
 dpkg-deb -xv libdpp-10.0.22-linux-x64.deb DPP_
 mv DPP_/usr DPP
 rm -rf DPP_
-sudo apt install g++ gdb cmake zlib1g-dev libarchive-dev libssl-dev
+sudo apt -y install g++ gdb cmake zlib1g-dev libarchive-dev libssl-dev
