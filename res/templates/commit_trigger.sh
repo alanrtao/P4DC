652461856bb0abb31fcb913ac7708e06b3610b08
Alan Tao <alanrtao@outlook.com>
2023-02-26 21:44:43 -0500

    Update commit_trigger.sh

--- res/templates/commit_trigger.sh
+++ res/templates/commit_trigger.sh
@@ -1,37 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
-# Author: Alan Tao, 23/02/06
-
+# Author: Alan Tao, 23/02/16
 # Creates a HTTPS request to the webhook whenever a commit is made,
 # Changelist descriptions are directly relayed, meaning no special
 # handlers or Pull Request markings and etc. within the script. It
 # is P4DC that parses these commits and actually creates threads.
 
-webhook="__webhook__"
-
-function webhook() {
-    curl \
-        -i \
-        -H "Accept: application/json" \
-        -H "Content-Type: application/json" \
-        -X POST \
-        --data "{\"content\":\"${1}\",\"embeds\":[]}" \
-        $webhook
+webhook_call() {
+    curl "__webhook__" \
+        -H "Accept: application/json" -H "Content-Type: application/json" -X POST -d "$(echo "$1")"
 }
 
 changelist="$1"
 workspace="$2"
 user="$3"
 
-msg=":satellite: New commit by \`$user\` titled \`$changelist\` (submitted from `$workspace`)\n\`\`\`"
+P4CMD=/usr/bin/p4
+P4DCTICKET=/opt/perforce/p4dc_ticket
+LOGIN="(p4 login -a -p \"<username>\" | tee /opt/perforce/p4dc_ticket) && sed -i -E '/^Enter/d' /opt/perforce/p4dc_ticket && chmod +644 /opt/perforce/p4dc_ticket"
 
-p4 fstat -e "$changelist" //... | while read -r line
-do
-    name=$(echo "$line" | sed 's/[\. ]\+\([^ ]\+\) .\+/\1/')
-    msg="$msg$name, "
-done
-msg="$msg\`\`\`"
-
-webhook $msg
-
+[[ -f $P4DCTICKET ]] || { echo "No P4DC ticket set in ${P4DCTICKET}. Please run \` $LOGIN \` on the server with the appropriate username and password." >&2; exit 1; }
+msg=":satellite:"
+msg+="$( \
+    $P4CMD -P "$(cat "$P4DCTICKET")" describe -a -s -m 80 "$changelist" 2>&1 | \
+    sed -E \
+        -e 's/^\.\.\./> .../g' \
+        -e 's/^\t/> /g' \
+        -e '/^Change/s/$/**/' \
+        -e '/^Change/s/^/**/' \
+        -e 's/\t/  /g' \
+        -e 's/"/\\"/g' \
+        -e 's/\$//g' | \
+    sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g' \
+    )"
+json="{\"username\":\"P4DC Buddy\",\"content\":\"$msg\"}"
+webhook_call "$json"
 exit 0
